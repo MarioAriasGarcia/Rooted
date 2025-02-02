e9e49baf628e9fd5beabf0ec468c3ffbe32dd183
Pedro-Gavin <pgavio00@estudiantes.unileon.es>
2025-02-02 11:32:43 +0100

    tutorial reproduce video

--- app/src/main/java/com/rooted/view/TutorialesActivity.java
+++ app/src/main/java/com/rooted/view/TutorialesActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.rooted.R;
 
 public class TutorialesActivity extends AppCompatActivity {
+    private boolean isPlaying = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,26 @@ public class TutorialesActivity extends AppCompatActivity {
 
         int userId = getIntent().getIntExtra("user_id", -1);
         String nombreUsuario = getIntent().getStringExtra("username");
+
+        VideoView videoView = findViewById(R.id.videoView);
+        String path = "android.resource://" + getPackageName() + "/" + R.raw.prueba2;
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isPlaying) {
+                    videoView.start();
+                    isPlaying = true;
+                } else {
+                    videoView.pause();
+                    isPlaying = false;
+                }
+            }
+        });
 
         Button volverMenuButton = findViewById(R.id.volver_menu_button);
         volverMenuButton.setOnClickListener(new View.OnClickListener() {
